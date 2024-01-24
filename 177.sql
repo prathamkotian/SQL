7d1bafef9651bbf3eb9f4654bd0c0d22ccd4f0c4
prathamkotian <kotianpratham9@gmail.com>
2024-01-24 19:30:14 +0530

    Nth Highest Salary

--- 177.sql
+++ 177.sql
@@ -2,6 +2,8 @@
 -- Declare a variable M and set it to N - 1. This is because the LIMIT clause uses a zero-based index,
 -- so the Nth highest salary corresponds to the (N-1)th row in the result set.
 -- Offset is used to specify from which row we want the data to retrieve
+-- Instead of LIMIT 1 OFFSET M, we can also use LIMIT M,1 
+-- LIMIT M,1 means select one row after Mth row. means Mth highest 
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
     declare M INT;
